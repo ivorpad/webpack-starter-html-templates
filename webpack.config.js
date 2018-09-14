867db812e543a8785003fd323cb55aa7a4a2ce0e
ivorpad <ivorjpc@gmail.com>
2018-09-14 14:56:32 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const webpack = require("webpack")
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+
+module.exports = () => ({
+  entry: "./src/js/scripts.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle.js"
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/main.css"
+    }),
+    new BrowserSyncPlugin({
+      injectChanges: true,
+      host: "localhost",
+      port: 3000,
+      files: ["./dist/*.html"],
+      server: { baseDir: ["dist"] }
+    }),
+    new WriteFilePlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new CopyWebpackPlugin([{ 
+      from: './src/*.html', to: './[name].html'
+    }])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              minimize: true
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: function() {
+                return [require("precss"), require("autoprefixer")];
+              }
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      }
+    ]
+  }
+});
